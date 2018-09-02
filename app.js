4058e5661bd7069a63b5712f0fe0e09be92a33af
Nishtha Malhotra <nishthamalhotra@gmail.com>
2018-09-02 12:21:53 -0400

    React setup

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
 module.exports = app;
 
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use(express.json());
 app.use('/api/users', require('./api/users'));
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.use((req, res, next) => {
     let error = new Error("Could not find the resource");
